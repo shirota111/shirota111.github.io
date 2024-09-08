75e67bb36354d21c473e0eb6793bbc2835128bbc
shirota111 <2210014974@campus.ouj.ac.jp>
2024-09-08 09:52:10 +0900

    module2.js の更新

--- module2.js
+++ module2.js
@@ -1,1 +1,7 @@
-a
+// module2.js
+
+// module1から呼び出される関数
+function handleEventInModule2(selectedDay) {
+    console.log(`Selected day from module1: ${selectedDay}`);
+    // ここで他の処理を追加することができます（例: 選択した日にタスクを追加する）
+}
