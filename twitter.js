6d66f035ffd320852e02d4f58f902e5329667918
shirota111 <2210014974@campus.ouj.ac.jp>
2024-09-18 17:05:38 +0900

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 // Bearer Token (セキュリティ上、環境変数から取得するのが推奨されます)
-const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAAOmvwEAAAAA8rOpAos4IwHfvpuYLlWO0G%2FHhX0%3DktYwVDdEnO1xoSNv7SqpP3WqKdyTsO7P4WZcPbhqstxZLAqBqD';
+const bearerToken = '';
 
 async function fetchUserData(username) {
   try {
